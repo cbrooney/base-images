091f1bbfe53bd46d3808a543972d599c600bed77
Cristian Bruni <bruni@auxmoney.com>
2023-02-01 19:18:59 +0100

    php8: start switching to php8

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,3 +1,3 @@
 # build prod base
-docker build . -f php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --target prod-base --tag ghcr.io/cbrooney/base-php-fpm-prod:latest
+docker build . -f php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:8.0.27-fpm-buster" --target prod-base --tag ghcr.io/cbrooney/base-php-fpm-prod:latest
 docker push ghcr.io/cbrooney/base-php-fpm-prod:latest
