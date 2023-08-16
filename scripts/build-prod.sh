5a3cfacb4d1fe3749e6db7414ceb247e165f9188
Cristian Bruni <bruni@auxmoney.com>
2023-08-16 20:47:28 +0200

    php8.2: adapt shell scripts

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,3 +1,3 @@
 # build prod base
-docker build . -f php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:8.0.27-fpm-buster" --target prod-base --tag ghcr.io/cbrooney/base-php-fpm-prod:latest
+docker build . -f php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:8.2.7-fpm-buster" --target prod-base --tag ghcr.io/cbrooney/base-php-fpm-prod:latest
 docker push ghcr.io/cbrooney/base-php-fpm-prod:latest
